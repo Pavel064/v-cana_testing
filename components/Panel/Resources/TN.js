8e270f04a90d1e8bc637969100ef24f627e25a11
valyukhov <alexzed@bk.ru>
2024-04-17 17:12:51 +0300

    feat: :sparkles: added tn and tw

--- components/Panel/Resources/TN.js
+++ components/Panel/Resources/TN.js
@@ -22,6 +22,7 @@ function TN({ config, url, toolName }) {
   const [currentScrollVerse, setCurrentScrollVerse] = useRecoilState(currentVerse)
   const [tnotes, setTnotes] = useState([])
   const { isLoading, data } = useGetResource({ config, url })
+  const [isLoadingTN, setIsLoadingTN] = useState(false)
   const { extraTNotes, setTnotes: updateTnotes } = useQuotesTranslation({
     domain: process.env.NEXT_PUBLIC_NODE_HOST ?? 'https://git.door43.org',
     book: config.reference.book,
@@ -38,6 +39,7 @@ function TN({ config, url, toolName }) {
   })
   useEffect(() => {
     if (extraTNotes) {
+      setIsLoadingTN(true)
       function filterNotes(dataArray) {
         const notesByVerse = {}
         if (!dataArray) return
@@ -56,12 +58,9 @@ function TN({ config, url, toolName }) {
         return notesByVerse
       }
 
-      // const _data = []
-      // for (const el of extraTNotes) {
-      //   filterNotes(el, el.verse, _data)
-      // }
       const newNotes = filterNotes(extraTNotes)
       setTnotes(newNotes)
+      setIsLoadingTN(false)
     }
   }, [extraTNotes])
 
@@ -72,45 +71,43 @@ function TN({ config, url, toolName }) {
   }, [data, updateTnotes])
 
   return (
-    <>
-      <TNotes
-        tnotes={tnotes}
-        nodeContentBack={
-          <span>
-            <Back className="w-8 stroke-th-primary-200" />
-          </span>
-        }
-        classes={{
-          content: {
-            container:
-              'absolute top-0 bottom-0 pr-2 ,bg-th-secondary-10 overflow-auto left-0 right-0',
-            header: 'sticky flex top-0 pb-4 bg-th-secondary-10',
-            backButton:
-              'w-fit h-fit p-1 mr-2.5 cursor-pointer hover:opacity-70 rounded-full bg-th-secondary-100',
-            title: 'font-bold text-xl mt-1',
-            text: 'markdown-body',
-          },
-          main: 'relative h-full',
-          list: {
-            verseNumber: 'text-2xl',
-            container:
-              'divide-y divide-th-text-primary divide-dashed h-full overflow-auto',
-            verseBlock: 'pl-7 flex-1',
-            currentNote: 'bg-th-secondary-100',
-            note: 'p-2 cursor-pointer rounded-lg hover:bg-th-secondary-100',
-            verseWrapper: 'p-4 flex mx-4',
-          },
-        }}
-        nodeLoading={<Placeholder />}
-        isLoading={isLoading}
-        scrollTopOffset={20}
-        startHighlightIds={checkLSVal('highlightIds', {}, 'object')}
-        currentScrollVerse={currentScrollVerse}
-        toolId={toolName}
-        idContainerScroll={config.idContainerScroll}
-        setCurrentScrollVerse={setCurrentScrollVerse}
-      />
-    </>
+    <TNotes
+      tnotes={tnotes}
+      nodeContentBack={
+        <span>
+          <Back className="w-8 stroke-th-primary-200" />
+        </span>
+      }
+      classes={{
+        content: {
+          container:
+            'absolute top-0 bottom-0 pr-2 ,bg-th-secondary-10 overflow-auto left-0 right-0',
+          header: 'sticky flex top-0 pb-4 bg-th-secondary-10',
+          backButton:
+            'w-fit h-fit p-1 mr-2.5 cursor-pointer hover:opacity-70 rounded-full bg-th-secondary-100',
+          title: 'font-bold text-xl mt-1',
+          text: 'markdown-body',
+        },
+        main: 'relative h-full',
+        list: {
+          verseNumber: 'text-2xl',
+          container: 'divide-y divide-th-text-primary divide-dashed h-full overflow-auto',
+          verseBlock: 'pl-7 flex-1',
+          currentNote: 'bg-th-secondary-100',
+          note: 'p-2 cursor-pointer rounded-lg hover:bg-th-secondary-100',
+          verseWrapper: 'p-4 flex mx-4',
+        },
+      }}
+      nodeLoading={<Placeholder />}
+      isLoading={isLoadingTN || isLoading}
+      scrollTopOffset={20}
+      startHighlightIds={checkLSVal('highlightIds', {}, 'object')}
+      currentScrollVerse={currentScrollVerse}
+      toolId={toolName}
+      idContainerScroll={config.idContainerScroll}
+      setCurrentScrollVerse={setCurrentScrollVerse}
+      delayScroll={500}
+    />
   )
 }
 
--- components/Panel/Resources/TWL.js
+++ components/Panel/Resources/TWL.js
@@ -10,12 +10,29 @@ import { useGetResource, useScroll } from 'utils/hooks'
 import { checkLSVal, filterNotes, getWords } from 'utils/helper'
 import { getFile } from 'utils/apiHelper'
 import Down from 'public/arrow-down.svg'
+import Back from 'public/left.svg'
+import { currentVerse } from 'components/state/atoms'
+import dynamic from 'next/dynamic'
+import { useRecoilState } from 'recoil'
+import { Disclosure } from '@headlessui/react'
+
+const TWords = dynamic(() => import('@texttree/v-cana-rcl').then((mod) => mod.TWords), {
+  ssr: false,
+})
 
 function TWL({ config, url, toolName }) {
-  const [item, setItem] = useState(null)
+  const [currentScrollVerse, setCurrentScrollVerse] = useRecoilState(currentVerse)
   const { isLoading, data } = useGetResource({ config, url })
+  const [word, setWord] = useState(null)
   const [wordObjects, setWordObjects] = useState([])
   const [isLoadingTW, setIsLoadingTW] = useState(false)
+  const [filter, setFilter] = useState(() => {
+    return checkLSVal('filter_words', 'disabled', 'string')
+  })
+  useEffect(() => {
+    localStorage.setItem('filter_words', filter)
+  }, [filter])
+
   useEffect(() => {
     const getData = async () => {
       setIsLoadingTW(true)
@@ -66,109 +83,59 @@ function TWL({ config, url, toolName }) {
 
   return (
     <>
-      <div className="relative h-full">
-        <TNTWLContent setItem={setItem} item={item} />
-        <TWLList
-          setItem={setItem}
-          data={wordObjects}
-          toolName={toolName}
-          isLoading={isLoadingTW || isLoading}
-        />
-      </div>
+      {!word && (
+        <div className="text-center mb-2">
+          {<FilterRepeated filter={filter} setFilter={setFilter} />}
+        </div>
+      )}
+      <TWords
+        twords={wordObjects}
+        nodeContentBack={
+          <span>
+            <Back className="w-8 stroke-th-primary-200" />
+          </span>
+        }
+        classes={{
+          main: 'relative h-full',
+          content: {
+            container:
+              'absolute top-0 bottom-0 pr-2 ,bg-th-secondary-10 overflow-auto left-0 right-0',
+            header: 'sticky flex top-0 pb-4 bg-th-secondary-10',
+            backButton:
+              'w-fit h-fit p-1 mr-2.5 cursor-pointer hover:opacity-70 rounded-full bg-th-secondary-100',
+            title: 'font-bold text-xl mt-1',
+            text: 'markdown-body',
+          },
+          list: {
+            verseNumber: 'text-2xl',
+            container:
+              'divide-y divide-th-text-primary divide-dashed h-full overflow-auto',
+            verseBlock: 'pl-7 flex-1',
+            currentWord: 'bg-th-secondary-100',
+            word: 'p-2 cursor-pointer rounded-lg hover:bg-th-secondary-100',
+            verseWrapper: 'p-4 flex mx-4',
+            filtered: 'text-th-secondary-300',
+          },
+        }}
+        nodeLoading={<Placeholder />}
+        isLoading={isLoadingTW || isLoading}
+        scrollTopOffset={20}
+        startHighlightIds={checkLSVal('highlightIds', {}, 'object')}
+        currentScrollVerse={currentScrollVerse}
+        toolId={toolName}
+        idContainerScroll={config.idContainerScroll}
+        setCurrentScrollVerse={setCurrentScrollVerse}
+        filter={filter}
+        word={word}
+        setWord={setWord}
+      />
     </>
   )
 }
 
 export default TWL
 
-function TWLList({ setItem, data, toolName, isLoading }) {
-  const [verses, setVerses] = useState([])
-
-  const [filter, setFilter] = useState(() => {
-    return checkLSVal('filter_words', 'disabled', 'string')
-  })
-  const { highlightId, handleSaveScroll } = useScroll({
-    toolName,
-    isLoading,
-    idPrefix: 'idtwl',
-  })
-
-  useEffect(() => {
-    localStorage.setItem('filter_words', filter)
-  }, [filter])
-
-  useEffect(() => {
-    if (data) {
-      setVerses(Object.entries(data))
-    }
-  }, [data])
-
-  return (
-    <div
-      id={`container_${toolName}`}
-      className={`divide-y divide-th-text-primary divide-dashed h-full overflow-auto ${
-        isLoading ? 'px-4' : ''
-      }`}
-    >
-      <div className="text-center mb-2">
-        {<FilterRepeated filter={filter} setFilter={setFilter} />}
-      </div>
-      {isLoading ? (
-        <div className="pt-4 pr-4">
-          <Placeholder />
-        </div>
-      ) : (
-        verses?.map(([verseNumber, words], verseIndex) => {
-          return (
-            <div key={verseIndex} className="p-4 flex mx-4" id={'idtwl' + verseNumber}>
-              <div className="text-2xl">{verseNumber}</div>
-              <div className="pl-7 flex-1">
-                <ul>
-                  {words?.map((item, index) => {
-                    let itemFilter
-                    switch (filter) {
-                      case 'disabled':
-                        itemFilter = false
-                        break
-                      case 'verse':
-                        itemFilter = item.isRepeatedInVerse
-                        break
-                      case 'book':
-                        itemFilter = item.isRepeatedInBook
-                        break
-                      default:
-                        break
-                    }
-
-                    return (
-                      <li
-                        key={index}
-                        id={'id' + item.id}
-                        className={`p-2 rounded-lg cursor-pointer ${
-                          itemFilter ? 'text-th-secondary-300' : ''
-                        } hover:bg-th-secondary-100
-                      ${highlightId === 'id' + item.id ? 'bg-th-secondary-100' : ''}
-                      `}
-                        onClick={() => {
-                          handleSaveScroll(verseNumber, item.id)
-                          setItem({ text: item.text, title: item.title })
-                        }}
-                      >
-                        <ReactMarkdown>{item.title}</ReactMarkdown>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            </div>
-          )
-        })
-      )}
-    </div>
-  )
-}
-
-function FilterRepeated({ setFilter, filter }) {
+function FilterRepeated({ setFilter, filter, isOpen = false }) {
   const { t } = useTranslation('common')
   const options = [
     { value: 'verse', name: t('ByVerse') },
@@ -177,23 +144,41 @@ function FilterRepeated({ setFilter, filter }) {
   ]
 
   return (
-    <div className="flex items-center justify-center gap-2">
-      <div className="hidden sm:block md:w-1/2">{t('FilterRepeatedWords')}</div>
-      <div className="relative w-full sm:w-1/2 mr-2">
-        <select
-          className="input-primary appearance-none truncate"
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-          style={{ padding: '10px' }}
-        >
-          {options?.map((option) => (
-            <option className="mr-2" value={option.value} key={option.value}>
-              {option.name}
-            </option>
-          ))}
-        </select>
-        <Down className="w-5 h-5 absolute -translate-y-1/2 top-1/2 right-4 stroke-th-text-primary pointer-events-none" />
-      </div>
-    </div>
+    <Disclosure defaultOpen={isOpen}>
+      {({ open }) => (
+        <>
+          <Disclosure.Panel>
+            <div className="flex items-center justify-center gap-2">
+              <div className="hidden sm:block md:w-1/2">{t('FilterRepeatedWords')}</div>
+              <div className="relative w-full sm:w-1/2 mr-2">
+                <select
+                  className="input-primary appearance-none truncate"
+                  value={filter}
+                  onChange={(e) => setFilter(e.target.value)}
+                  style={{ padding: '10px' }}
+                >
+                  {options?.map((option) => (
+                    <option className="mr-2" value={option.value} key={option.value}>
+                      {option.name}
+                    </option>
+                  ))}
+                </select>
+                <Down className="w-5 h-5 absolute -translate-y-1/2 top-1/2 right-4 stroke-th-text-primary pointer-events-none" />
+              </div>
+            </div>
+          </Disclosure.Panel>
+          <Disclosure.Button>
+            <div className="flex gap-1 justify-center w-full pt-3 border-t border-th-secondary-300 text-th-secondary-300">
+              <span>{t(open ? 'Hide' : 'Open')}</span>
+              <Down
+                className={`w-6 max-w-[1.5rem] stroke-th-secondary-300 ${
+                  open ? 'rotate-180 transform' : ''
+                }`}
+              />
+            </div>
+          </Disclosure.Button>
+        </>
+      )}
+    </Disclosure>
   )
 }
