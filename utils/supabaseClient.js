ad98bf0007a6eaaaf6b94fead1d5b22bd9b298da
Pavel <braurs64@gmail.com>
2024-04-26 16:48:42 +0400

    build: remove @supabase/ssr

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,12 +1,16 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { useState } from 'react'
 
 function useSupabaseClient() {
-  const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const [supabaseClient] = useState(() =>
+    createPagesBrowserClient({
+      cookieOptions: {
+        name: process.env.NEXT_PUBLIC_COOKIE_NAME ?? 'sb-vcana-cookies',
+        sameSite: 'lax',
+      },
+    })
   )
-
-  return supabase
+  return supabaseClient
 }
 
 export default useSupabaseClient
--- utils/supabaseServer.js
+++ utils/supabaseServer.js
@@ -1,33 +1,27 @@
-import { createServerClient } from '@supabase/ssr'
-import { getCookie } from 'cookies-next'
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
 
 const supabaseApi = async ({ req, res }) => {
-  const cookieStore = {
-    get(name) {
-      return getCookie(name, { req, res })
-    },
-  }
-
-  const supabaseServerApi = createServerClient(
-    process.env.SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: cookieStore,
-    }
-  )
-
+  let supabaseServerApi
   try {
+    supabaseServerApi = createPagesServerClient(
+      { req, res },
+      {
+        supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        supabaseUrl: process.env.SUPABASE_URL,
+        cookieOptions: {
+          name: process.env.NEXT_PUBLIC_COOKIE_NAME ?? 'sb-vcana-cookies',
+        },
+      }
+    )
     const {
       data: { session },
     } = await supabaseServerApi.auth.getSession()
-
     if (!session) {
       throw Error('Access denied!')
     }
   } catch (error) {
     throw error
   }
-
   return supabaseServerApi
 }
 
